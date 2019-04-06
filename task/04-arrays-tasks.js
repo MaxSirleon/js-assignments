38330cf6e3a85d8ae057cbcb97138afa721e38ce
MaxSirleon <48217534+MaxSirleon@users.noreply.github.com>
2019-04-07 01:21:27 +0300

    Update 04-arrays-tasks.js

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -304,7 +304,7 @@ function get3TopItems(arr) {
  *   [ -1, 0, 1 ] => 1
  *   [ 1, 2, 3]   => 3
  *   [ null, 1, 'elephant' ] => 1
- *   [ 1, '2' ] => 1
+ *   [ 1, 2 ] => 2
  */
 function getPositivesCount(arr) {
    return arr.reduce((element1, element2) => element1 + (element2 > 0), 0);
