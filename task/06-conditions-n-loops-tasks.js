90a39594920d3108f0e6252f3a19a57ad1ae541c
MaxSirleon <48217534+MaxSirleon@users.noreply.github.com>
2019-05-18 17:14:26 +0300

    Update 06-conditions-n-loops-tasks.js

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -30,7 +30,10 @@
  *
  */
 function getFizzBuzz(num) {
-    throw new Error('Not implemented');
+   if (num % 15 === 0) return 'FizzBuzz';
+   if (num % 3 === 0) return 'Fizz';
+   if (num % 5 === 0) return 'Buzz';
+   return num;
 }
 
 
@@ -123,7 +126,7 @@ function isTriangle(a,b,c) {
  *  
  */
 function doRectanglesOverlap(rect1, rect2) {
-    throw new Error('Not implemented');
+    return rect1.top + rect1.height > rect2.top && rect1.left + rect1.width > rect2.left && rect2.top + rect2.height > rect1.top && rect2.left + rect2.width > rect1.left;
 }
 
 
